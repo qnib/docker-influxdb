0eef1bdca8d7b104e7915ba569d6ff53cb6353ce
Christian Kniep <ckniep@gaikai.com>
2016-03-09 14:55:15 +0100

    update start-script

--- opt/qnib/influxdb/bin/start.sh
+++ opt/qnib/influxdb/bin/start.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
+#!/usr/local/bin/dumb-init /bin/bash
 
 PIDFILE=/var/run/influxdb.pid
 
 if [ "X${INFLUXDB_CLUSTER_PEERS}" != "X" ];then
  	INFLUXD_JOIN="-join ${INFLUXDB_CLUSTER_PEERS}"
 fi
-if [ "X${INFLUXDB_HOSTNAME}" != "X" ];then
-    INFLUXD_HOST="${INFLUXDB_HOSTNAME}"
-else
-    INFLUXD_HOST="$(hostname -f)"
-fi
-INFLUXD_OPTS="-hostname ${INFLUXD_HOST}:8088 ${INFLUXD_JOIN}"
 
 ## Check if eth0 already exists
 ADDR=eth0
@@ -27,11 +21,4 @@ function stop_influxdb {
 }
 trap "stop_influxdb" SIGINT SIGTERM
 
-influxd -pidfile ${PIDFILE} -config /etc/influxdb/influxdb.conf ${INFLUXD_OPTS} &
-sleep 3
-#curl -sG http://localhost:8086/query --data-urlencode "q=CREATE DATABASE carbon"
-#curl -sG http://localhost:8086/query --data-urlencode "q=CREATE DATABASE influxdb"
-
-while [ -f ${PIDFILE} ];do
-    sleep 1
-done
+influxd -pidfile ${PIDFILE} -config /etc/influxdb/influxdb.conf ${INFLUXD_OPTS} 
