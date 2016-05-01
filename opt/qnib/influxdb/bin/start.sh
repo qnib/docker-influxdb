a5b7ab82abb86420012241117a1f088cff28b4d8
Christian Kniep <christian@qnib.org>
2016-05-01 13:49:08 +0200

    adjust start-process

--- opt/qnib/influxdb/bin/start.sh
+++ opt/qnib/influxdb/bin/start.sh
@@ -6,6 +6,19 @@ if [ "X${INFLUXDB_CLUSTER_PEERS}" != "X" ];then
  	INFLUXD_JOIN="-join ${INFLUXDB_CLUSTER_PEERS}"
 fi
 
+CONSUL_RELOAD=0
+if [ ${INFLUXDB_OPENTSDB_ENABLED} != "true" ];then
+    rm -f /etc/consul.d/influxdb_opentsdb.json
+    CONSUL_RELOAD=1
+fi
+if [ ${INFLUXDB_GRAPHITE_ENABLED} != "true" ];then
+    rm -f /etc/consul.d/influxdb_graphite.json
+    CONSUL_RELOAD=1
+fi
+if [ ${CONSUL_RELOAD} -eq 1 ];then
+    consul reload
+fi
+
 ## Check if eth0 already exists
 ADDR=eth0
 ip addr show ${ADDR} > /dev/null
