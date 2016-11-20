c60150604678a798ae81134eed958feb13ba102f
Christian Kniep <christian@qnib.org>
2016-11-20 16:50:15 +0100

    sleep 5 seconds before creating databases

--- opt/qnib/influxdb/bin/init.sh
+++ opt/qnib/influxdb/bin/init.sh
@@ -5,9 +5,11 @@ set -ex
 source /opt/qnib/consul/etc/bash_functions.sh
 
 wait_for_srv influxdb
+sleep 5
 
 ## Create databases
 for db in $(echo ${INFLUXDB_DATABASES} |sed -e 's/,/ /g');do
+    echo "> influx -host localhost -password root -username root -execute 'CREATE DATABASE ${db}'"
     influx -host localhost -password root -username root -execute "CREATE DATABASE ${db}"
 done
 
